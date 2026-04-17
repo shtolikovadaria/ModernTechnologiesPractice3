51efac523d5ce075275c32757a0c869145eafdf8
242800 <shtolikovadasha@gmail.com>
2026-04-17 19:36:40 +0300

    update 1.2

--- part1/part1_2/GradeChecker.java
+++ part1/part1_2/GradeChecker.java
@@ -6,14 +6,58 @@ public class GradeChecker {
         // TODO: реализуйте вариант через классический switch.
         // Подсказка: удобно сначала вычислить "десяток" score / 10.
         // ▼ ВАШ КОД ЗДЕСЬ ▼
-        return "TODO";
+        int category;
+
+        if (score >= 90 && score <= 100) {
+            category = 1;
+        } else if (score >= 80 && score <= 89) {
+            category = 2;
+        } else if (score >= 70 && score <= 79) {
+            category = 3;
+        } else if (score >= 60 && score <= 69) {
+            category = 4;
+        } else {
+            category = 5;
+        }
+        switch (category) {
+            case 1:
+                return "Отлично (A)";
+            case 2:
+                return "Хорошо (B)";
+            case 3:
+                return "Удовлетворительно (C)";
+            case 4:
+                return "Слабо (D)";
+            default:
+                return "Неудовлетворительно (F)";
+
+        }
         // ▲ КОНЕЦ ВАШЕГО КОДА ▲
     }
 
     public static String getGradeArrow(int score) {
         // TODO: реализуйте вариант через switch -> (Java 14+).
         // ▼ ВАШ КОД ЗДЕСЬ ▼
-        return "TODO";
+        int category;
+
+        if (score >= 90 && score <= 100) {
+            category = 1;
+        } else if (score >= 80 && score <= 89) {
+            category = 2;
+        } else if (score >= 70 && score <= 79) {
+            category = 3;
+        } else if (score >= 60 && score <= 69) {
+            category = 4;
+        } else {
+            category = 5;
+        }
+        return switch (category) {
+            case 1 -> "Отлично (A)";
+            case 2 -> "Хорошо (B)";
+            case 3 -> "Удовлетворительно (C)";
+            case 4 -> "Слабо (D)";
+            default -> "Неудовлетворительно (F)";
+        };
         // ▲ КОНЕЦ ВАШЕГО КОДА ▲
     }
 
