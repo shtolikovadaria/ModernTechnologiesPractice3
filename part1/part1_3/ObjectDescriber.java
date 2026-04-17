fa48feacde5ca1177f88e85a54c3bfd3daa27cb2
242800 <shtolikovadasha@gmail.com>
2026-04-17 19:42:15 +0300

    update 1.3

--- part1/part1_3/ObjectDescriber.java
+++ part1/part1_3/ObjectDescriber.java
@@ -7,7 +7,36 @@ public class ObjectDescriber {
         // Подсказка 1: обработайте null отдельным case.
         // Подсказка 2: для Integer укажите "положительное" или "не положительное".
         // ▼ ВАШ КОД ЗДЕСЬ ▼
-        return "TODO";
+        return switch (obj) {
+            case null -> "null значение";
+
+            case Integer i -> {
+                if (i > 0) {
+                    yield "Integer: положительное (" + i + ")";
+                } else {
+                    yield "Integer: не положительное (" + i + ")";
+                }
+            }
+
+            case String s when s.isEmpty() -> "String: пустая строка";
+            case String s -> "String: \"" + s + "\" (длина: " + s.length() + ")";
+
+            case Double d -> "Double: " + d;
+
+            case int[] arr -> {
+                StringBuilder sb = new StringBuilder("int[]: [");
+                for (int i = 0; i < arr.length; i++) {
+                    sb.append(arr[i]);
+                    if (i < arr.length - 1) {
+                        sb.append(", ");
+                    }
+                }
+                sb.append("] (длина: ").append(arr.length).append(")");
+                yield sb.toString();
+            }
+
+            default -> "Другой объект: " + obj.getClass().getSimpleName() + " (" + obj + ")";
+        };
         // ▲ КОНЕЦ ВАШЕГО КОДА ▲
     }
 
